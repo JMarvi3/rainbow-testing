6db3c7548417764c2526ad3afdf47b47668f9c4f
James Marvin <47727350+JMarvi3@users.noreply.github.com>
2024-04-17 19:13:02 -0400

    Merged in Aryamaan's changes. Worked on decode_uv_delta.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from setuptools import setup, Extension
 from os.path import join
+import numpy
 
 setup(
-    package_dir={'rainbow': 'rainbow'},
+    packages=['rainbow'],
     py_modules=['rainbow.__init__'],
-    ext_modules=[Extension('rainbow._rainbow', [join('rainbow', '_rainbow.c')])],
+    ext_modules=[Extension('rainbow._rainbow',
+                           [join('rainbow', '_rainbow.c')], include_dirs=[numpy.get_include()])]
 )
